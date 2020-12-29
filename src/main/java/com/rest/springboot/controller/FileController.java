5c04acc76f7036ab7f8a50695147095b8a24743d
Prabhat Agarwal <prabhat.agarwal0605@gmail.com>
2020-12-30 00:00:05 +0530

    added controller lever request mapping

--- src/main/java/com/rest/springboot/controller/FileController.java
+++ src/main/java/com/rest/springboot/controller/FileController.java
@@ -8,39 +8,41 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rest.springboot.model.FileDetail;
 import com.rest.springboot.service.FileService;
 
 @RestController
+@RequestMapping("/files")
 public class FileController {
 
 	@Autowired
 	private FileService fileService;
 
-	@GetMapping("/files")
+	@GetMapping
 	public List<FileDetail> getAllFiles() {
 		return fileService.getAllFiles();
 	}
 
-	@GetMapping("/files/{id}")
+	@GetMapping("/{id}")
 	public FileDetail getFile(@PathVariable("id") Long id) {
 		System.out.println(id);
 		return fileService.getFile(id);
 	}
 
-	@PostMapping("/files")
+	@PostMapping
 	public FileDetail saveFileDetails(@RequestBody FileDetail fileDetail) {
 		return fileService.saveFileDetails(fileDetail);
 	}
 
-	@PostMapping("/files/{id}")
+	@PostMapping("/{id}")
 	public FileDetail updateFileDetails(@PathVariable("id") Long id, @RequestBody FileDetail fileDetail) throws Exception {
 		return fileService.updateFileDetails(id, fileDetail);
 	}
 
-	@DeleteMapping("/files/{id}")
+	@DeleteMapping("/{id}")
 	public void deleteFileDetail(@PathVariable("id") Long id) {
 		fileService.deleteFileDetail(id);
 	}
