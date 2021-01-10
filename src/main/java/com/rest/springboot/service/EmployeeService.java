74b56e4297d14d7b9b463178a50446bb88d5c0b7
Prabhat Agarwal <prabhat.agarwal0605@gmail.com>
2021-01-10 23:58:05 +0530

    updated metho names

--- src/main/java/com/rest/springboot/service/EmployeeService.java
+++ src/main/java/com/rest/springboot/service/EmployeeService.java
@@ -16,19 +16,19 @@ import com.rest.springboot.repository.EmployeeRepository;
 @Service
 public class EmployeeService {
 	@Autowired
-	private EmployeeRepository fileRepository;
+	private EmployeeRepository employeeRepository;
 	private ModelMapper mapper = new ModelMapper();
 
 	public List<EmployeeDetails> getAllEmployees() {
-		Iterable<Employee> fileEntities = fileRepository.findAll();
+		Iterable<Employee> employees = employeeRepository.findAll();
 		Type type = new TypeToken<List<EmployeeDetails>>() {
 		}.getType();
-		List<EmployeeDetails> EmployeeDetailss = mapper.map(fileEntities, type);
+		List<EmployeeDetails> EmployeeDetailss = mapper.map(employees, type);
 		return EmployeeDetailss;
 	}
 
 	public EmployeeDetails getEmployee(Long id) {
-		Optional<Employee> EmployeeEntity = fileRepository.findById(id);
+		Optional<Employee> EmployeeEntity = employeeRepository.findById(id);
 		if (EmployeeEntity.isPresent()) {
 			return mapper.map(EmployeeEntity.get(), EmployeeDetails.class);
 		}
@@ -38,23 +38,23 @@ public class EmployeeService {
 	public EmployeeDetails saveEmployeeDetails(EmployeeDetails EmployeeDetails) {
 		Employee employee = mapper.map(EmployeeDetails, Employee.class);
 		System.out.println(employee.getId());
-		Employee savedEmployee = fileRepository.save(employee);
+		Employee savedEmployee = employeeRepository.save(employee);
 		EmployeeDetails.setId(savedEmployee.getId());
 		return EmployeeDetails;
 	}
 
 	public EmployeeDetails updateEmployeeDetails(Long id, EmployeeDetails EmployeeDetails) throws Exception {
-		boolean isExist = fileRepository.existsById(id);
+		boolean isExist = employeeRepository.existsById(id);
 		if (isExist) {
 			EmployeeDetails.setId(id);
 			Employee employee = mapper.map(EmployeeDetails, Employee.class);
-			employee = fileRepository.save(employee);
+			employee = employeeRepository.save(employee);
 			return mapper.map(employee, EmployeeDetails.class);
 		}
-		throw new Exception("File not exist with id " + id);
+		throw new Exception("Employee not exist with id " + id);
 	}
 
 	public void deleteEmployeeDetails(Long id) {
-		fileRepository.deleteById(id);
+		employeeRepository.deleteById(id);
 	}
 }
